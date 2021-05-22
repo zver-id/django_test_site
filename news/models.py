f82e2b65f72d5e7f8c06875809e4113a7ab48d27
zver-id-lin <helix999@mail.ru>
2021-05-22 09:25:28 +0400

    added admin

--- news/models.py
+++ news/models.py
@@ -2,12 +2,18 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=False)
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Создано')
+    updated_at = models.DateField(auto_now=True, verbose_name='Изменено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото')
+    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at', 'title']
+
