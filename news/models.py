7cd17c5b3675ec22da5218fded5314612f968c3e
zver-id-lin <helix999@mail.ru>
2021-05-22 10:37:05 +0400

    updated created_at to DateTimeField

--- news/models.py
+++ news/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='Наименование')
     content = models.TextField(blank=True, verbose_name='Контент')
-    created_at = models.DateField(auto_now_add=True, verbose_name='Создано')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
     updated_at = models.DateField(auto_now=True, verbose_name='Изменено')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
 
     def __str__(self):
